3289d52e584e0d63246ab86f436c3564bc0228c1
Vitalii Filchenko <vfilchenko95@gmail.com>
2022-10-22 17:09:45 +0300

    Add products card

---
+++ src/components/ProductPageComponent/ProductCard.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
+
+const Card = styled(Box)(() => ({
+  outline: "1px solid #1976d2",
+  borderRadius: "4px",
+  backgroundColor: "#1976d2",
+  color: "white",
+  textAlign: "center",
+}));
+
+const Price = styled(Typography)(() => ({
+  fontSize: "12px",
+}));
+
+const ProductCard = ({ product }) => {
+  return (
+    <Card>
+      <img
+        src={product.thumbnail}
+        alt={product.title}
+        height="250px"
+        width="250px"
+      />
+      <Box sx={{}}>
+        <Typography>{product.title}</Typography>
+        <Price>Price: {product.price}</Price>
+      </Box>
+    </Card>
+  );
+};
+
+export default ProductCard;
--- src/components/ProductPageComponent/index.js
+++ src/components/ProductPageComponent/index.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
+
+import ProductCard from "./ProductCard";
+import { useGetProductsQuery } from "../../services/AuthAPI";
+
+const PageWrap = styled(Box)(() => ({
+  height: "100vh",
+}));
+
+const CardsWrap = styled(Box)(() => ({
+  width: "850px",
+  display: "flex",
+  flexWrap: "wrap",
+  marginLeft: "auto",
+  marginRight: "auto",
+  padding:"10px",
+  gap: "15px",
+}));
 
 const ProductPageComponent = () => {
-  return <></>;
+  const [pages, setPages] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [products, setProducts] = useState(null);
+  const { data } = useGetProductsQuery(currentPage);
+
+  useEffect(() => {
+    if (data) {
+      setPages(data.last_page);
+      setProducts(data.data);
+    }
+  }, [data]);
+
+  return (
+    <PageWrap>
+      {products && (
+        <CardsWrap>
+          {products.map((el) => (
+            <ProductCard key={el.id} product={el} />
+          ))}
+        </CardsWrap>
+      )}
+    </PageWrap>
+  );
 };
 
 export default ProductPageComponent;
