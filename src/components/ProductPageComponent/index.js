81ef27b40fd0f747ab6076dd4fcf2885f9597edb
Vitalii Filchenko <vfilchenko95@gmail.com>
2022-10-22 17:23:38 +0300

    Add pagination

--- src/components/ProductPageComponent/index.js
+++ src/components/ProductPageComponent/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import Box from "@mui/material/Box";
 import { styled } from "@mui/material/styles";
+import Pagination from "@mui/material/Pagination";
 
 import ProductCard from "./ProductCard";
 import { useGetProductsQuery } from "../../services/AuthAPI";
@@ -11,20 +12,27 @@ const PageWrap = styled(Box)(() => ({
 }));
 
 const CardsWrap = styled(Box)(() => ({
-  width: "850px",
+  width: "800px",
   display: "flex",
   flexWrap: "wrap",
   marginLeft: "auto",
   marginRight: "auto",
-  padding:"10px",
+  padding: "10px",
   gap: "15px",
+  boxSizing: "border-box",
+}));
+
+const StuledPagination = styled(Pagination)(() => ({
+  marginTop: "20px",
+  marginLeft: "auto",
+  marginRight: "auto",
 }));
 
 const ProductPageComponent = () => {
   const [pages, setPages] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
   const [products, setProducts] = useState(null);
-  const { data } = useGetProductsQuery(currentPage);
+  const { data } = useGetProductsQuery({ page: currentPage });
 
   useEffect(() => {
     if (data) {
@@ -33,14 +41,26 @@ const ProductPageComponent = () => {
     }
   }, [data]);
 
+  const handleChangePage = (event, value) => {
+    setCurrentPage(value);
+  };
+
   return (
     <PageWrap>
       {products && (
-        <CardsWrap>
-          {products.map((el) => (
-            <ProductCard key={el.id} product={el} />
-          ))}
-        </CardsWrap>
+        <>
+          <CardsWrap>
+            {products.map((el) => (
+              <ProductCard key={el.id} product={el} />
+            ))}
+            <StuledPagination
+              count={pages}
+              page={currentPage}
+              onChange={handleChangePage}
+              color="primary"
+            />
+          </CardsWrap>
+        </>
       )}
     </PageWrap>
   );
